19073738d6a1f6d42d0ec18b1c5d59867c3350c2
SofiaGGarcia <sofiaggarcia92@gmail.com>
2022-12-01 20:54:39 -0300

    Calculadora de Tami

--- calculadora.js
+++ calculadora.js
@@ -1,29 +1,27 @@
-var n1, n2, suma, resta, mult, div;
-n1 = parseInt(prompt("Ingrese el primer numero"));
-n2 = parseInt(prompt("Ingrese el segundo numero"));
-///////VALIDACIONES //////////////////////////////////
-while (isNaN(n1 && n2)){
-    n1=parseInt(prompt("Ingrese número 1"))
-    n2=parseInt(prompt("Ingrese número 2"))
-}
-if (n1 <= 0 || n2 <= 0){
-    n1=parseInt(prompt("Ingrese número 1"))
-    n2=parseInt(prompt("Ingrese número 2"))
-}
-////////////////////
+function calculator(){
+    let num1 = parseFloat(document.getElementById('number1').value);
+    let num2 = parseFloat(document.getElementById('number2').value);
 
-suma = n1 + n2;
-resta = n1 - n2;
-mult = n1 * n2;
-div = n1 / n2;
-if (mult == 0){
-    mult = 1;}
-alert("Suma: " + suma);
-alert("Resta: " + resta);
-alert("Multiplicacion: " + mult);
-alert("Division: " + div);
+    let operation = document.getElementById('operators').value;
 
-console.log("Suma: " + suma);
-console.log("Resta: " + resta);
-console.log("Multiplicacion: " + mult);
-console.log("Division: " + div);
+    if (operation === 'plus')
+     {
+    document.getElementById('result').value = (num1+num2);  
+    }
+
+    if (operation === 'minus') 
+    {
+    document.getElementById('result').value = (num1-num2);  
+    }
+
+    if (operation === 'division') 
+    {
+    document.getElementById('result').value = (num1/num2);  
+    }
+
+    if (operation === 'mult') 
+    {
+    document.getElementById('result').value = (num1*num2);  
+    }
+    
+}
